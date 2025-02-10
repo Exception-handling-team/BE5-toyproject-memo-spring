398f538aa1417e86ec227441001745bba440be89
SDHSeoulTech <dongseoultech@gmail.com>
2025-02-10 10:15:05 +0900

    커밋

--- src/main/java/com/example/toyproject_spring/global/security/SecurityConfig.java
+++ src/main/java/com/example/toyproject_spring/global/security/SecurityConfig.java
@@ -27,6 +27,8 @@ public class SecurityConfig {
                                 .permitAll()
                                 .requestMatchers("/auth/**")
                                 .permitAll()
+                                .requestMatchers("/error")
+                                .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
